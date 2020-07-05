52be0c917adb6464e7c8f867a0848cd3f662a8a4
stellaLight <stellamaria.mendonca@techjini.com>
2020-07-05 13:05:55 +0530

    second commiting made isotope filter and footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,9 +34,10 @@ export class HomeComponent implements OnInit {
       proName: 'Meats',
       img: 'assets/img/product/categories/cat-5.jpg'
     }
-  ]
+  ];
 
   SlideOptions = { items: 4, dots: true, nav: true, navigation: true };
+  public istopeValue;
   // CarouselOptions = { items: 3, dots: true, nav: true };
 
   constructor() { }
@@ -45,7 +46,50 @@ export class HomeComponent implements OnInit {
 
   }
 
+  public selectIsotop(event) {
 
+    this.istopeValue = event.target.dataset.target;
+    const items = document.querySelector('.items').children;
+	  const target = event.target.dataset.target;
+
+    for(let k=0; k<items.length; k++){
+        items[k]['style'].opacity = '0';
+        items[k]['style'].transform = 'scale(0)';
+        setTimeout(function() {
+        // items[k]['style'].display='none';
+          items[k]['style'].height='0';
+          items[k]['style'].width='0';
+          items[k]['style'].padding='0';
+          items[k]['style'].visibility='hidden';
+        }, 1000);
+
+
+      if(items[k].getAttribute('data-id')==target){
+        items[k]['style'].opacity='1';
+        items[k]['style'].transform='scale(1)';
+        setTimeout(function() {
+        // items[k]['style'].display='inline-block';
+          items[k]['style'].height='25%';
+          items[k]['style'].width='25%';
+          items[k]['style'].padding='15px';
+          items[k]['style'].visibility='visible';
+        }, 1000)
+
+      }
+      if(target=='all'){
+        items[k]['style'].opacity='1';
+        items[k]['style'].transform='scale(1)';
+        setTimeout(function() {
+        // items[k]['style'].display='inline-block';
+        items[k]['style'].height='25%';
+        items[k]['style'].width='25%';
+        items[k]['style'].padding='15px';
+        items[k]['style'].visibility='visible';
+        }, 1000)
+      }
+    }
+
+	}
 
 
 
